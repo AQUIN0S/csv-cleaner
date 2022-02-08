7d4103c705f83dbe68f49e74eb45234a58efb909
AQUIN0S <danny.manski135@gmail.com>
2022-02-09 01:34:39 +1300

    Try some things out, going back to basics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
-use std::io::{self, BufRead, LineWriter, Write};
+use std::io::{self, BufRead, BufReader, LineWriter, Write};
 use std::path::Path;
 use std::path::PathBuf;
+use std::str::FromStr;
 use std::{env, fs};
 use structopt::StructOpt;
 
@@ -18,56 +19,45 @@ enum CleaningResult<T> {
 )]
 struct CliOpts {
     /// Input directory to find CSV files in
-    #[structopt(parse(from_os_str))]
-    input_directory: PathBuf,
+    // #[structopt(parse(from_os_str))]
+    // input_directory: PathBuf,
 
-    /// Use Linux style line endings ("\n"). If not set, use Windows style ("\r\n").
+    /// Expect linux style line endings in the input ("\n"). If not set, defaults to Windows style ("\r\n").
     #[structopt(short, long)]
-    linux: bool,
+    read_linux: bool,
 
-    /// Quotes are to be unescaped. I can't really think of a situation where this would be set though...
+    /// Write with linux style line endings to the output ("\n"). If not set, defaults to Windows style ("\r\n").
     #[structopt(short, long)]
-    unescaped: bool,
+    write_linux: bool,
 
-    /// Don't close quotes in cells. Usually best not to set this as it will result in less clean data, though it will
-    /// also mean the result is closer to the original.
-    #[structopt(short, long)]
-    unclosed_quotes: bool,
+    /// The character to use as a separator (default ',')
+    #[structopt(short, long, default_value = ",")]
+    separator: char,
 
-    /// Remove quotes altogether. This may or may not help...
-    #[structopt(short, long)]
-    remove_quotes: bool,
-
-    /// Tabs are used instead of commas
-    #[structopt(short, long)]
-    tabs: bool,
+    /// The size of the data chunks being read at once. Larger chunks mean less reads, resulting in faster results, but requires more memory. Defaults to 1MB (1e6 Bytes)
+    #[structopt(short, long, default_value = "1_000_000")]
+    chunk_size: u32,
 }
 
 use CleaningResult::{Clean, Dirty};
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
 fn run_through_directory(options: CliOpts) {
+    let input_directory = PathBuf::from_str("C:\\Downloads\\").expect("Couldn't parse path");
+
     println!(
         "Directory: {}",
-        options.input_directory.as_os_str().to_str().unwrap()
+        input_directory.as_os_str().to_str().unwrap()
     );
     println!();
 
-    let mut output_directory = PathBuf::from(&options.input_directory);
+    let mut output_directory = PathBuf::from(&input_directory);
     output_directory.push("out");
 
-    println!("input: {}", options.input_directory.display());
+    println!("input: {}", input_directory.display());
     println!("output: {}", output_directory.display());
 
     let mut full_dir_path = env::current_dir().unwrap();
-    full_dir_path.push(&options.input_directory);
+    full_dir_path.push(&input_directory);
 
     // Replace the <input_dir>/out/ directory, making for a clean output
     let _ = fs::remove_dir_all(&output_directory); // Cleanish way of getting rid of an unneeded warning on this line
@@ -75,7 +65,7 @@ fn run_through_directory(options: CliOpts) {
 
     let mut problems: Vec<String> = Vec::new();
 
-    for entry in match fs::read_dir(&options.input_directory) {
+    for entry in match fs::read_dir(input_directory) {
         Ok(read_dir) => read_dir,
         Err(_) => {
             eprintln!(
@@ -109,153 +99,47 @@ fn run_through_directory(options: CliOpts) {
     }
 }
 
+/// Use the provided reader to parse a single csv line. An assumption made is that every call to this
+/// is made at the very start of a new line.
+fn read_csv_line<'a>(options: &'a CliOpts, reader: BufReader<File>, column_count: Option<u32>) {}
+
 fn clean_file<'a>(options: &'a CliOpts, file_path: &'a Path) -> Vec<String> {
+    let input_directory = PathBuf::from_str("C:\\Downloads\\").expect("Couldn't parse path");
+
     println!();
     println!("File to read: {}", file_path.as_os_str().to_str().unwrap());
     let mut read_first_line = false;
     let mut num_columns: usize = 0;
     let mut line_number = 0;
 
-    let mut output_file_path = PathBuf::from(options.input_directory.as_path());
+    let mut output_file_path = PathBuf::from(input_directory.as_path());
     output_file_path.push("out");
     output_file_path.push(file_path.file_name().unwrap());
 
     let mut problems: Vec<String> = Vec::new();
 
-    if let Ok(lines) = read_lines(file_path) {
-        let output_file = File::create(output_file_path).unwrap();
-        let mut file_writer = LineWriter::new(output_file);
-
-        for result in lines {
-            line_number += 1;
-
-            if let Ok(line) = result {
-                if read_first_line {
-                    if line.matches(if options.tabs { '\t' } else { ',' }).count() + 1
-                        != num_columns
-                    {
-                        let error = format!("Please review line {} of file {}, number of columns in row doesn't match the number of columns in header! This line has not been written into the output file",
-                            line_number,
-                            file_path.as_os_str().to_str().unwrap());
-                        eprintln!("{}", error);
-                        problems.push(error);
-                        continue;
-                    }
-                } else {
-                    num_columns = line.matches(if options.tabs { '\t' } else { ',' }).count() + 1;
-                    println!("Number of columns: {}", num_columns);
-                    read_first_line = true;
-                }
-                let mut cleaned_line = match clean_line(options, line) {
-                    Clean(cleaned_line) => cleaned_line,
-                    Dirty(cleaned_line) => {
-                        println!();
-                        println!("Needed to clean line {}:", line_number);
-                        println!("{}", cleaned_line);
-                        cleaned_line
-                    }
-                };
-
-                cleaned_line += if options.linux { "\n" } else { "\r\n" };
-
-                match file_writer.write_all(cleaned_line.as_bytes()) {
-                    Err(e) => {
-                        let error = format!(
-                            "Error writing line {} of file {} due to {}",
-                            line_number,
-                            file_path.file_name().unwrap().to_str().unwrap(),
-                            e.to_string()
-                        );
-                        eprintln!("{}", error);
-                        problems.push(error);
-                    }
-                    _ => {}
-                };
-            } else {
-                let error = format!(
-                    "Error in reading line {} of file \"{}\". Line not written into output file, please review",
-                    line_number,
-                    file_path.as_os_str().to_str().unwrap()
-                );
-                eprintln!("{}", error);
-                problems.push(error);
-            }
+    let file = match match File::open(file_path) {
+        Ok(file) => Some(file),
+        Err(e) => {
+            let error = format!(
+                "Couldn't open file {} due to {}",
+                file_path.as_os_str().to_str().unwrap(),
+                e,
+            );
+            eprintln!("{}", error);
+            problems.push(error);
+            None
         }
+    } {
+        Some(file) => file,
+        None => return problems,
+    };
 
-        match file_writer.flush() {
-            Err(e) => {
-                eprintln!(
-                    "Error writing line {} of file {} due to {}",
-                    line_number,
-                    file_path.file_name().unwrap().to_str().unwrap(),
-                    e.to_string()
-                );
+    let reader = BufReader::new(file);
 
-                let error = format!(
-                    "Error writing line {} of file {} due to {}",
-                    line_number,
-                    file_path.file_name().unwrap().to_str().unwrap(),
-                    e.to_string()
-                );
-                eprintln!("{}", error);
-                problems.push(error);
-            }
-            _ => {}
-        };
-    } else {
-        let problem = format!("Could not read file {}", file_path.display());
-        eprintln!("{}", problem);
-        problems.push(problem);
-    }
+    let mut header_count = 0;
 
     problems
-}
-
-fn clean_line(opts: &CliOpts, line: String) -> CleaningResult<String> {
-    let mut result = String::new();
-
-    let mut open_quote = false;
-    let mut was_dirty = false;
-    let mut prev_char: Option<char> = None;
-
-    for character in line.chars() {
-        if character == '\"' {
-            if opts.remove_quotes {
-                was_dirty = true;
-                continue;
-            }
-            if prev_char == None || prev_char == Some(if opts.tabs { '\t' } else { ',' }) {
-                // TODO: Implement a thing
-            } else {
-                // TODO: Implement another thing
-            }
-            open_quote = !open_quote;
-            result += if opts.unescaped { "" } else { "\\" };
-        } else if character == if opts.tabs { '\t' } else { ',' }
-            && open_quote
-            && !opts.unclosed_quotes
-        {
-            result += if opts.unescaped { "\"" } else { "\\\"" };
-            open_quote = false;
-            was_dirty = true;
-        } else if character == '\n' || character == '\r' {
-            if opts.unescaped || opts.remove_quotes || opts.unclosed_quotes {
-                was_dirty = true;
-                continue;
-            }
-        } else if character == '\\' {
-            was_dirty = true;
-            continue;
-        }
-        result += character.to_string().as_str();
-        prev_char = Some(character);
-    }
-
-    if was_dirty {
-        Dirty(result)
-    } else {
-        Clean(result)
-    }
 }
 
 fn main() {
